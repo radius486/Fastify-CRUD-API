a39f158a266f761ca3a1ef1fc8f7bab3c273886f
Rodion Grishaev <radius486@gmail.com>
2026-03-21 23:49:49 +0300

    feat: Swagger and project structure.

--- src/server.js
+++ src/server.js
@@ -1,83 +1,17 @@
+
 import Fastify from 'fastify';
+import swaggerPlugin from './plugins/swagger.js';
+import productRoutes from './routes/products.js';
 
-const fastify = Fastify({
-  logger: true
-})
-
-let products = [
-  {
-    id: 'id_1',
-    name: 'Product 2',
-    description: 'Some product',
-    price: 100,
-    category: 'electronics',
-    inStock: true,
-  },
-];
-
-fastify.get('/api/products', async (request, reply) => {
-  return products;
-});
-
-fastify.get('/api/products/:id', async (request, reply) => {
-  const { id } = request.params;
-  const product = products.find(p => p.id === id);
-
-  if (!product) {
-    return reply.status(404).send({ error: 'Product not found' });
-  }
-
-  return product;
-});
-
-fastify.post('/api/products', async (request, reply) => {
-  const { name, description, price, category, inStock } = request.body;
-
-  const newProduct = {
-    id: `id_${products.length + 1}`,
-    name,
-    description,
-    price,
-    category,
-    inStock,
-  };
-
-  products.push(newProduct);
-
-  return reply.status(201).send(newProduct);
-});
-
-fastify.put('/api/products/:id', async (request, reply) => {
-  const { id } = request.params;
-  const { name, description, price, category, inStock } = request.body;
-  const index = products.findIndex(p => p.id === id);
-
-  if (index === -1) {
-    return reply.status(404).send({ error: 'Product not found' });
-  }
-
-  products[index] = { id: id, name, description, price, category, inStock };
-
-  return products[index];
-});
-
-fastify.delete('/api/products/:id', async (request, reply) => {
-  const { id } = request.params;
-  const initialLength = products.length;
-
-  products = products.filter(p => p.id !== id);
-
-  if (products.length === initialLength) {
-    return reply.status(404).send({ error: 'Product not found' });
-  }
-
-  return reply.status(204).send();
-});
+const fastify = Fastify({ logger: true });
 
 const start = async () => {
   try {
+    await fastify.register(swaggerPlugin);
+    await fastify.register(productRoutes);
+
     await fastify.listen({ port: 3000 });
-    console.log(`Server is running at http://localhost:3000`);
+    console.log('Docs: http://localhost:3000/docs');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
