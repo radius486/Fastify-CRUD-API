17a31508514647745fa5509173832a6ce968eb28
Rodion Grishaev <radius486@gmail.com>
2026-03-21 23:58:41 +0300

    feat: Use PORT from .env.

--- src/server.js
+++ src/server.js
@@ -2,16 +2,36 @@
 import Fastify from 'fastify';
 import swaggerPlugin from './plugins/swagger.js';
 import productRoutes from './routes/products.js';
+import fastifyEnv from '@fastify/env';
 
 const fastify = Fastify({ logger: true });
+
+const schema = {
+  type: 'object',
+  required: ['PORT'],
+  properties: {
+    PORT: {
+      type: 'integer',
+      default: 3000
+    }
+  }
+};
+
+const options = {
+  confKey: 'config', // переменные будут доступны в fastify.config
+  schema: schema,
+  dotenv: true // загружать из .env файла
+};
 
 const start = async () => {
   try {
     await fastify.register(swaggerPlugin);
     await fastify.register(productRoutes);
+    await fastify.register(fastifyEnv, options);
 
-    await fastify.listen({ port: 3000 });
-    console.log('Docs: http://localhost:3000/docs');
+    await fastify.listen({ port: fastify.config.PORT });
+    console.log(`Server listening on port ${fastify.config.PORT}`);
+    console.log(`Docs: http://localhost:${fastify.config.PORT}/docs`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
